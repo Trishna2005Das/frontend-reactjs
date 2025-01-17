4f37ab4177f7c3cbdd5f1dc1502932886bca0b93
Trishna2005Das <trishnadas2005@gmail.com>
2025-01-17 10:55:01 +0530

    bg-changer-completed

--- bg-changer-project1/src/App.jsx
+++ bg-changer-project1/src/App.jsx
@@ -1,35 +1,38 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  
+  const [color, setColor] = useState('black')
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div className="w-full h-screen duration-300"
+    style={{backgroundColor: color}}>
+   
+    <div className='fixed flex justify-center items-center bottom-12 inset-x-0 px-2'>
+    <div className='flex flex-wrap gap-2 shadow-lg bg-white p-2 rounded-3xl'>
+      <button 
+      onClick={() => setColor('olive')}
+      className="text-white outline-none px-4 py-1 rounded-full" style={{backgroundColor:'olive'}}>olive</button>
+      <button 
+      onClick={() => setColor('red')}
+      className="text-white outline-none px-4 py-1 rounded-full" style={{backgroundColor:'red'}}>red</button>
+      <button 
+      onClick={() => setColor('green')}
+      className="text-white outline-none px-4 py-1 rounded-full" style={{backgroundColor:'green'}}>green</button>
+      <button 
+      onClick={() => setColor('blue')}
+      className="text-white outline-none px-4 py-1 rounded-full" style={{backgroundColor:'blue'}}>blue</button>
+      
+      </div> 
+    </div>
+    </div>
     </>
   )
 }
 
 export default App
+
+// what does w-full do?->full width
+// what does h-screen do?->full height
+// what does duration-300 do?->change the duration of the transition
